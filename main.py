d4580928972bcff9acc15e058656b0054a207a30
June Paolo Tabequero <tabequeroj.ranz@gmail.com>
2022-06-09 23:08:01 +0800

    add items to txt file

--- main.py
+++ main.py
@@ -11,12 +11,11 @@ window.geometry("800x600")
 window.resizable(False, False)
 global add_e1
 global add_e2
-global e3
+global add_e3
 global e4
 global total
 global balance
 global items_dict
-dot_counter = 0
 
 style = ttk.Style()
 style.theme_use("clam")
@@ -25,167 +24,231 @@ total = StringVar()
 balance = IntVar()
 addinput_itemname = StringVar()
 addinput_price = StringVar()
-items_dict = {}
+addinput_quantity = StringVar()
 
-def only_numbers(char):
+def only_numbersdecimal(char):
     return char.isdigit() or char == '.'
 
-validation = window.register(only_numbers)
+def only_numbers(char):
+    return char.isdigit()
+
+price_validation = window.register(only_numbersdecimal)
+quantity_validation = window.register(only_numbers)
 
 def add_back():
     main_window()
-    addinput_itemname.set("")
-    addinput_price.set("")
+    addinput_itemname.set('')
+    addinput_price.set('')
+    addinput_quantity.set('')
 
 def add_items():
     itemname_iscorrect = False
     price_iscorrect = False
+    quantity_iscorrect = False
+    items_dict = {}
 
     if not addinput_itemname.get() or len(addinput_itemname.get().strip()) == 0:
         itemname_iscorrect = False
-        messagebox.showinfo("Error", "Please enter item name")
+        messagebox.showinfo('Error', 'Please enter item name')
     else:
         itemname_iscorrect = True
         item_name = addinput_itemname.get().strip().title()
 
-    if not addinput_price.get() or addinput_price.get() == "." or float(addinput_price.get()) == 0:
+    if not addinput_price.get() or addinput_price.get() == '.' or float(addinput_price.get()) == 0:
         price_iscorrect = False
-        messagebox.showinfo("Error", "Please enter item price")
+        messagebox.showinfo('Error', 'Please enter item price')
     else:
         price_iscorrect = True
         item_price = float(addinput_price.get())
     
-    if itemname_iscorrect and price_iscorrect:
+    if not addinput_quantity.get() or float(addinput_quantity.get()) == 0:
+        quantity_iscorrect = False
+        messagebox.showinfo('Error', 'Please enter item quantity')
+    else:
+        quantity_iscorrect = True
+        item_quantity = int(addinput_quantity.get())
+    
+    if itemname_iscorrect and price_iscorrect and quantity_iscorrect:
         if item_name in items_dict:
-            messagebox.showinfo("Error", "Item already exists")
+            messagebox.showinfo('Error', 'Item already exists')
         else:
-            items_dict[item_name] = item_price
+            items_dict[item_name] = [{"quantity":item_quantity}, {"price":item_price}]
             messagebox.showinfo("Success", "Item added")
             add_back()
-            addinput_itemname.set("")
-            addinput_price.set("")
+            addinput_itemname.set('')
+            addinput_price.set('')
+            addinput_quantity.set('')
+        add_items_to_file(items_dict, clear=False)
+
+def add_items_to_file(items_dict: dict, clear: bool):
+    if clear:
+        f = open('inventory.txt', 'w')
+        f.close()
+        with open('inventory.txt', 'a') as file:
+            for item in items_dict:
+                file.write(f"{item}: {items_dict[item]}")
+                file.write('\n')
+        return
+    invItems = getInvItems()
+
+    for item in invItems:
+        if item in items_dict:
+            items_dict[item] += invItems[item]
+    with open('inventory.txt', 'a') as file:
+        for item in items_dict:
+            file.write(f"{item}: {items_dict[item]}")
+            file.write('\n')
+
+def getInvItems():
+    invItems = {}
+    with open('inventory.txt', 'r') as file:
+        for line in file:
+            line = line.replace('\n','').split(' ')
+            item_name, item_quantity, item_price = line[0].replace(':',''), line[2].replace('},',''), line[4].replace('}]','')
+            invItems.update({item_name: [{"quantity":int(item_quantity)}, {"price":float(item_price)}]})
+
+    return invItems
 
 def add_window():
     add_frame = Frame(window, width=800, height=600)
-    add_frame.tk_setPalette(background="#E9EEF3", foreground="#2C4C71")
+    add_frame.tk_setPalette(background='#E9EEF3', foreground='#2C4C71')
     add_frame.grid(row=0, column=0, sticky=NW)
     add_frame.propagate(0)
     add_frame.update()
 
     Label(
         add_frame,
-        text="Add Item(s)",
-        font="Arial 18 bold").place(
+        text='Add Item(s)',
+        font='Arial 18 bold').place(
             relx=.5,
             y=40,
             anchor=CENTER)
     Button(
         add_frame,
-        text="Back",
+        text='Back',
         command=add_back,
         height=2,
         width=5,
-        background="#2C4C71",
-        foreground="#E9EEF3").place(
+        background='#2C4C71',
+        foreground='#E9EEF3').place(
             relx=.03,
             y=40)
     Label(
         add_frame,
-        text="Item Name",
-        font="Arial 14 bold").place(
+        text='Item Name',
+        font='Arial 14 bold').place(
             x=60,
-            y=210)
+            y=160)
     add_e1 = Entry(
         add_frame,
         width=30,
         textvariable=addinput_itemname,
-        font="Arial 14",
-        foreground="black",
+        font='Arial 14',
+        foreground='black',
         highlightthickness=1,
-        highlightbackground="#2C4C71",
-        highlightcolor="#2C4C71").place(
+        highlightbackground='#2C4C71',
+        highlightcolor='#2C4C71').place(
             x=60,
-            y=250)
+            y=200)
     Label(
         add_frame,
-        text="Price",
-        font="Arial 14 bold").place(
+        text='Price',
+        font='Arial 14 bold').place(
             x=450,
-            y=210)
+            y=160)
     add_e2 = Entry(
         add_frame,
         width=25,
         textvariable=addinput_price,
-        font="Arial 14",
-        foreground="black",
+        font='Arial 14',
+        foreground='black',
         highlightthickness=1,
-        highlightbackground="#2C4C71",
-        highlightcolor="#2C4C71",
-        validate="key",
-        validatecommand=(validation, "%S")).place(
+        highlightbackground='#2C4C71',
+        highlightcolor='#2C4C71',
+        validate='key',
+        validatecommand=(price_validation, '%S')).place(
+            x=450,
+            y=200)
+    Label(
+        add_frame,
+        text='Quantity',
+        font='Arial 14 bold').place(
             x=450,
             y=250)
+    add_e3 = Entry(
+        add_frame,
+        width=10,
+        textvariable=addinput_quantity,
+        font='Arial 14',
+        foreground='black',
+        highlightthickness=1,
+        highlightbackground='#2C4C71',
+        highlightcolor='#2C4C71',
+        validate='key',
+        validatecommand=(quantity_validation, '%S')).place(
+            x=450,
+            y=290)
     Button(
         add_frame,
-        text="ADD",
+        text='ADD',
         command=add_items,
         height=8,
         width=30,
-        background="#2C4C71",
-        foreground="#E9EEF3").place(
+        background='#2C4C71',
+        foreground='#E9EEF3').place(
             relx=.5,
             y=490,
             anchor=CENTER)
 
 def view_window():
-    print("View")
+    print('View')
 
 def purchase_window():
-    print("Purchase")
+    print('Purchase')
 
 def main_window():
     main_frame = Frame(window, width=800, height=600)
-    main_frame.tk_setPalette(background="#446285", foreground="#A5B8CC")
+    main_frame.tk_setPalette(background='#446285', foreground='#A5B8CC')
     main_frame.grid(row=0, column=0, sticky=NW)
     main_frame.grid_propagate(0)
     main_frame.update()
     
     Label(
         main_frame,
-        text="Sales and Inventory System",
-        font="Arial 18 bold").place(relx=.5,
+        text='Sales and Inventory System',
+        font='Arial 18 bold').place(relx=.5,
         y=40,
         anchor=CENTER)
     Button(
         main_frame,
-        text="Add Item(s)",
+        text='Add Item(s)',
         command=add_window,
         height=8,
         width=30,
-        background="#A5B8CC",
-        foreground="black").place(
+        background='#A5B8CC',
+        foreground='black').place(
             relx=.5,
             y=190,
             anchor=CENTER)
     Button(
         main_frame,
-        text="View Item(s)",
+        text='View Item(s)',
         command=view_window,
         height=8,
         width=30,
-        background="#A5B8CC",
-        foreground="black").place(
+        background='#A5B8CC',
+        foreground='black').place(
             relx=.5,
             y=340,
             anchor=CENTER)
     Button(
         main_frame,
-        text="Purchase Item(s)",
+        text='Purchase Item(s)',
         command=purchase_window,
         height=8,
         width=30,
-        background="#A5B8CC",
-        foreground="black").place(
+        background='#A5B8CC',
+        foreground='black').place(
             relx=.5,
             y=490,
             anchor=CENTER)
